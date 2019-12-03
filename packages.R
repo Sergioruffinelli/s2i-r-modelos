678603d22f1ab8bb52c2238fe9c9a5f197fa3158
Sergio Ruffinelli <sergio@ruffinelli.com.ar>
2019-12-03 16:44:40 -0300

    correccion en modelo.R

--- packages.R
+++ packages.R
@@ -1,3 +1,7 @@
+
+install.packages('plumber',repo='https://cloud.r-project.org/')
+
+
 ip <- as.data.frame(installed.packages()[,c(1,3:4)])
 rownames(ip) <- NULL
 ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
